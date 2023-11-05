ffca618f21b3845560f6f3f879b83206742c5701
asdiop159752 <nxt159753@gmail.com>
2023-11-05 17:42:00 +0700

    update new

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        output: 'export',
         domains:["res.cloudinary.com"],
         remotePatterns: [
             {
