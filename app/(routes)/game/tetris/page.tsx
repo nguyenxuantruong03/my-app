d271ca852aac18c33764dc8e154b7430b1dcadde
asdiop159752 <nxt159753@gmail.com>
2023-11-30 09:27:23 +0700

    update ressponsove terit (left)

--- app/(routes)/game/tetris/page.tsx
+++ app/(routes)/game/tetris/page.tsx
@@ -7,7 +7,7 @@ export const revalidate = 86400;
 const TetrisPage = () => {
   return (
     <div className="relative">
-            <div className="absolute left-36 top-60"> 
+            <div className="absolute lg:left-16 xl:left-36 top-60"> 
       <div className="w-20 h-10 bg-amber-300 rounded-md flex items-center text-sm px-2 mx-auto my-1 font-semibold">Lưu ý<AlertTriangle className=" ml-1 h-5 w-5" /> </div>
       <div className="font-semibold w-48 rounded p-1 mx-auto bg-opacity-50 bg-gray-300 "> Để trải nghiệm tốt xem bảng điều khiên để di chuyển khối theo hướng.</div>
             </div>
