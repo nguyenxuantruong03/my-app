60b71a519ef959e07f9c2aaac37bb1b83d1a7d97
asdiop159752 <nxt159753@gmail.com>
2023-12-11 12:13:19 +0700

    bỏ đi use client trong layout

--- app/(routes)/category3/layout.tsx
+++ app/(routes)/category3/layout.tsx
@@ -1,9 +1,7 @@
-"use client"
 import { Nunito } from 'next/font/google';
 import type { Metadata } from "next";
 import Footer from '@/components/footer/footer';
 import Navbar from '@/components/navbar/navbar';
-import { useEffect, useState } from 'react';
 export const metadata: Metadata & { image: string } = {
   title: "Dây điện", 
   description: "Vật liệu xây dựng Xuân Trường",
@@ -17,15 +15,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
   return (
     <html lang="en">
       <body className={roboto.className}>
